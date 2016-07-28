ef774e913f8f4792a4215c553e64c71ca5270f67
Tiziano Di Condina <dicondinatiziano@gmail.com>
2016-07-28 12:13:04 +0200

    add new method

--- src/main/java/it/uniroma2/sapr/persistence/MySQLDbDAOFactory.java
+++ src/main/java/it/uniroma2/sapr/persistence/MySQLDbDAOFactory.java
@@ -33,7 +33,7 @@ public class MySQLDbDAOFactory extends DAOFactory {
 		try {
 			Class.forName(DRIVER).newInstance();
 			con = DriverManager.getConnection(DBURL, "root", "");
-                        //con = DriverManager.getConnection(DBURL, "dindi", "torvergata");
+			//con = DriverManager.getConnection(DBURL, "dindi", "torvergata");
 			return con;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
