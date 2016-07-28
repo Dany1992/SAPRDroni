ed5512093189008a5807de81ce0218bc96409db5
Dariutso <dario.onorati@hotmail.it>
2016-07-28 11:30:14 +0200

    inseriti get and set
    presto anche il resto

--- src/main/java/it/uniroma2/sapr/pojo/Device.java
+++ src/main/java/it/uniroma2/sapr/pojo/Device.java
@@ -16,4 +16,64 @@ public class Device {
     private int weight;
     private String producer;
     private String pilotLicense;
+
+    public Device(int idDevice, String model, String type, int weight, String producer, String pilotLicense) {
+        this.idDevice = idDevice;
+        this.model = model;
+        this.type = type;
+        this.weight = weight;
+        this.producer = producer;
+        this.pilotLicense = pilotLicense;
+    }
+
+    public int getIdDevice() {
+        return idDevice;
+    }
+
+    public void setIdDevice(int idDevice) {
+        this.idDevice = idDevice;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getPilotLicense() {
+        return pilotLicense;
+    }
+
+    public void setPilotLicense(String pilotLicense) {
+        this.pilotLicense = pilotLicense;
+    }
+    
+    
+    
 }
