1d2a49850f18c86c2c731dea0dcb74695a29d717
Tiziano Di Condina <dicondinatiziano@gmail.com>
2016-10-17 15:14:40 +0200

    Add update method, add checkElement in flightPlan and note class

--- src/main/java/it/uniroma2/sapr/pojo/FlightPlain.java
+++ src/main/java/it/uniroma2/sapr/pojo/FlightPlain.java
@@ -5,6 +5,8 @@
  */
 package it.uniroma2.sapr.pojo;
 
+import java.util.ArrayList;
+
 /**
  * Questa classe è l'oggetto che viene creato prelevando i dati dalla RequestFlightPlain.
  * Tale oggetto viene utilizzato per le operazioni CRUD sul db;
@@ -21,8 +23,9 @@ public class FlightPlain {
     private int idNote;
     private int idDevice;
     private String pilotLicense;
+    private ArrayList<CheckElement> checkList;
 
-    public FlightPlain(String destination, String departure, String dateDeparture, String timeDeparture, String nowArriving, int idSapr, int idNote, int idDevice, String pilotLicense) {
+    public FlightPlain(String destination, String departure, String dateDeparture, String timeDeparture, String nowArriving, int idSapr, int idNote, int idDevice, String pilotLicense, ArrayList<CheckElement> checkList) {
         this.destination = destination;
         this.departure = departure;
         this.dateDeparture = dateDeparture;
@@ -32,7 +35,8 @@ public class FlightPlain {
         this.idNote = idNote;
         this.idDevice = idDevice;
         this.pilotLicense = pilotLicense;
-    }
+        this.checkList = checkList;
+    } 
 
     public String getDestination() {
         return destination;
@@ -106,18 +110,27 @@ public class FlightPlain {
         this.pilotLicense = pilotLicense;
     }
 
+    public ArrayList<CheckElement> getCheckList() {
+        return checkList;
+    }
+
+    public void setCheckList(ArrayList<CheckElement> checkList) {
+        this.checkList = checkList;
+    }
+
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 59 * hash + (this.destination != null ? this.destination.hashCode() : 0);
-        hash = 59 * hash + (this.departure != null ? this.departure.hashCode() : 0);
-        hash = 59 * hash + (this.dateDeparture != null ? this.dateDeparture.hashCode() : 0);
-        hash = 59 * hash + (this.timeDeparture != null ? this.timeDeparture.hashCode() : 0);
-        hash = 59 * hash + (this.nowArriving != null ? this.nowArriving.hashCode() : 0);
-        hash = 59 * hash + this.idSapr;
-        hash = 59 * hash + this.idNote;
-        hash = 59 * hash + this.idDevice;
-        hash = 59 * hash + (this.pilotLicense != null ? this.pilotLicense.hashCode() : 0);
+        int hash = 5;
+        hash = 41 * hash + (this.destination != null ? this.destination.hashCode() : 0);
+        hash = 41 * hash + (this.departure != null ? this.departure.hashCode() : 0);
+        hash = 41 * hash + (this.dateDeparture != null ? this.dateDeparture.hashCode() : 0);
+        hash = 41 * hash + (this.timeDeparture != null ? this.timeDeparture.hashCode() : 0);
+        hash = 41 * hash + (this.nowArriving != null ? this.nowArriving.hashCode() : 0);
+        hash = 41 * hash + this.idSapr;
+        hash = 41 * hash + this.idNote;
+        hash = 41 * hash + this.idDevice;
+        hash = 41 * hash + (this.pilotLicense != null ? this.pilotLicense.hashCode() : 0);
+        hash = 41 * hash + (this.checkList != null ? this.checkList.hashCode() : 0);
         return hash;
     }
 
@@ -160,16 +173,16 @@ public class FlightPlain {
         if ((this.pilotLicense == null) ? (other.pilotLicense != null) : !this.pilotLicense.equals(other.pilotLicense)) {
             return false;
         }
+        if (this.checkList != other.checkList && (this.checkList == null || !this.checkList.equals(other.checkList))) {
+            return false;
+        }
         return true;
     }
 
     @Override
     public String toString() {
-        return "FlightPlain{" + "destination=" + destination + ", departure=" + departure + ", dateDeparture=" + dateDeparture + ", timeDeparture=" + timeDeparture + ", nowArriving=" + nowArriving + ", idSapr=" + idSapr + ", idNote=" + idNote + ", idDevice=" + idDevice + ", pilotLicense=" + pilotLicense + '}';
-    }
-    
-    
-    
-    
+        return "FlightPlain{" + "destination=" + destination + ", departure=" + departure + ", dateDeparture=" + dateDeparture + ", timeDeparture=" + timeDeparture + ", nowArriving=" + nowArriving + ", idSapr=" + idSapr + ", idNote=" + idNote + ", idDevice=" + idDevice + ", pilotLicense=" + pilotLicense + ", checkList=" + checkList + '}';
+    }  
+  
     
 }
