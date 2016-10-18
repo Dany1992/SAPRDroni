34700274b077bfb6741624d4d20cd64c26724099
Dariutso <dario.onorati@hotmail.it>
2016-10-18 15:23:04 +0200

    commento importNTISSIMO

--- src/main/java/it/uniroma2/sapr/persistence/DeviceDAO.java
+++ src/main/java/it/uniroma2/sapr/persistence/DeviceDAO.java
@@ -14,6 +14,7 @@ public interface DeviceDAO {
     public boolean insertDevice(Device device) throws SQLException;
     public boolean deleteDevice(Device device) throws SQLException;
     public boolean updateDevice(Device device) throws SQLException;
+    // in base ai parametri faccio la giusta select
     public ArrayList<ResponseDevice> selectDevice(String owner) throws SQLException;
     public ResponseDevice selectDevice(Device device) throws SQLException;
 }
